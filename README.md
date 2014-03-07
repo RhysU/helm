306dae033d2bd278251a03544562869b02f59047
Rhys Ulerich <rhys.ulerich@gmail.com>
2014-03-06 21:32:01 -0600

    Untested draft

--- README.md
+++ README.md
@@ -2,3 +2,24 @@ helm
 ====
 
 A header-only C99 proportional-integral-derivative (PID) controller.
+
+The controller, implemented wholly in [helm.h](helm.h), features:
+ * low pass filtering of the process derivative,
+ * windup protection,
+ * automatic reset on actuator saturation,
+ * anti-kick on setpoint change using "derivative on measurement",
+ * incremental output for bumpless manual-to-automatic transitions,
+ * a unified controller gain parameter,
+ * exposure of all independent physical time scales, and
+ * the ability to accommodate varying sample rate.
+
+<center><img src="helm.svg" width="95%"/></center>
+
+The design and nomenclature is based largely on Figure 10.11 of
+[Feedback Systems](http://www.worldcat.org/isbn/9781400828739) by
+Astrom and Murray.
+
+References
+----------
+
+TODO
---
+++ helm.h
@@ -0,0 +1,194 @@
+//--------------------------------------------------------------------------
+//
+// Copyright (C) 2014 Rhys Ulerich
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+//--------------------------------------------------------------------------
+
+#ifndef HELM_H
+#define HELM_H
+
+#include <assert.h>
+#include <math.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// TODO Document the continuous time and discretized equations
+// FIXME Check claims on no filtering with Tf = 1
+
+/** \file
+ * \brief A header-only C99 proportional-integral-derivative (PID) controller.
+ *
+ * This controller features
+ * <ul>
+ *   <li>low pass filtering of the process derivative,</li>
+ *   <li>windup protection,</li>
+ *   <li>automatic reset on actuator saturation,</li>
+ *   <li>anti-kick on setpoint change using "derivative on measurement",</li>
+ *   <li>incremental output for bumpless manual-to-automatic transitions,</li>
+ *   <li>a unified controller gain parameter,</li>
+ *   <li>exposure of all independent physical time scales, and</li>
+ *   <li>the ability to accommodate varying sample rate.</li>
+ * </ul>
+ * The design and nomenclature is based largely on Figure 10.11 of <a
+ * href="http://www.worldcat.org/isbn/9781400828739">Feedback Systems</a> by
+ * Astrom and Murray.
+ *
+ * Sample written with nomenclature from \ref helm_state and \ref helm_steady:
+ * \code
+ *   struct helm_state h;
+ *
+ *   // Set PID parameters from commonly given \c kp, \c ki, \c kt, and \c kd
+ *   helm_reset(h);
+ *   h->kp = kp;
+ *   h->Td = kd / h->kp;
+ *   h->Tf = h->Td / 10;  // Astrom and Murray p.308 suggests 2--20
+ *   h->Ti = h->kp / ki;
+ *   h->Tt = h->kp / kt;
+ *
+ *   // Enable automatic control and evolve
+ *   helm_approach(h);
+ *   for (int i = 0; i < N; ++i) {
+ *      y  = process(dt, u);
+ *      v += helm_steady(h, dt, r, u, v, y);
+ *      u  = actuate(dt, v);
+ *   }
+ *
+ *   // Disable controller and evolve
+ *   for (int i = 0; i < N; ++i) {  // E
+ *      y  = process(dt, u);
+ *      u  = actuate(dt, v);
+ *   }
+ *
+ *   // Re-enable automatic control and evolve
+ *   helm_approach(h);
+ *   for (int i = 0; i < N; ++i) {
+ *      y  = process(dt, u);
+ *      v += helm_steady(h, dt, r, u, v, y);
+ *      u  = actuate(dt, v);
+ *   }
+ * \endcode
+ */
+
+/**
+ * State for an incremental PID controller, including all tuning parameters.
+ */
+struct helm_state
+{
+    /**
+     * Controller tuning parameters.
+     *
+     * Gain has units of <code>u0 / y0</code>.
+     * Tt has units of time multiplied by <code>u0 / y0</code>.
+     * All other time scales possess units of time.
+     *
+     * Setting a time scale to \c INFINITY disables the associated term.
+     * 
+     * @{
+     */
+    double kp;  /**< Proportional gain modifying P, I, and D terms.  */
+    double Td;  /**< Time scale governing derivative action.         */
+    double Tf;  /**< Time scale filtering process observable for D.  */
+    double Ti;  /**< Time scale governing integral action.           */
+    double Tt;  /**< Time scale governing automatic reset.           */
+    /**@}*/
+
+    /**
+     * Internal state maintained between calls to \ref steady(...)
+     * @{
+     */
+    double y;   /**< Tracks instantaneous process observable. */
+    double f;   /**< Tracks filtered process observable.      */
+    /**@}*/
+};
+
+/**
+ * \brief Reset all tuning parameters, but \e not transient state.
+ *
+ * Resets gain to one and  disables filtering, integral action, and derivative
+ * action.  Enable those terms by setting the associated time scales.
+ */
+static inline
+void
+helm_reset(struct helm_state * const h)
+{
+    h->kp = 1;        // Unit gain
+    h->Td = INFINITY; // No derivative action
+    h->Tf = 1;        // No filtering
+    h->Ti = INFINITY; // No integral action
+    h->Tt = INFINITY; // No automatic reset
+}
+
+/**
+ * \brief Forget any transient state, but \e not tuning parameters.
+ *
+ * Necessary to achieve bumpless manual-to-automatic transitions
+ * before calling to \ref helm_steady after a period of manual control.
+ */
+static inline
+void
+helm_approach(struct helm_state * const h)
+{
+    assert(h->Td > 0);
+    assert(h->Tf > 0);
+    assert(h->Ti > 0);
+    assert(h->Tt > 0);
+    h->f = NAN;
+}
+
+/**
+ * \brief Find the control signal necessary to steady unsteady process y(t).
+ *
+ * \param[in,out] h  Tuning parameters and state maintained across invocations.
+ * \param[in]     dt Time since last samples collected.
+ * \param[in]     r  Reference value, often called the "setpoint".
+ * \param[in]     u  Actuator signal currently observed.
+ * \param[in]     v  Actuator signal currently requested.
+ * \param[in]     y  Observed process output to drive to \c r.
+ *
+ * \return Incremental suggested change to control signal \c v.
+ */
+static inline
+double
+helm_steady(struct helm_state * const h,
+            const double dt,
+            const double r,
+            const double u,
+            const double v,
+            const double y)
+{
+    double dy, df, dv = 0;
+
+    if (!isnan(y)) {                      // Avoid driving blind
+
+        if (isnan(h->f)) {                // Avoid startup kick
+            h->y = y;
+            h->f = y;
+        }
+
+        dy  = y - h->y;                   // Backward difference for y
+        df  = (dt / h->Tf)*(y - h->f);    // Filtered difference for y
+        dv += (r - y) / h->Ti;            // Action from integral control
+        dv += (u - v) / h->Tt;            // Action from automatic reset
+        dv *= dt;                         // Scale integral actions by time step
+        dv += (h->Td / h->Tf)*(df - dy);  // Action from derivative control
+        dv += /*dr=0*/ - dy;              // Action from proporational control
+        dv *= h->kp;                      // Scale by unified gain parameter
+
+        h->y  = y;                        // Track observable for next call
+        h->f += df;                       // Track filter for next call
+    }
+
+    return dv;
+}
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* HELM_H */
